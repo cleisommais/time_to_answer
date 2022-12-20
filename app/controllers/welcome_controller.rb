9dcabe4eb6577bd3bc0d270ef01fa22171ddef42
cleisommais <cleisommais@gmail.com>
2022-12-19 19:51:54 -0600

    Added devise auth and added two devise models, admin and profile

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,3 @@
 class WelcomeController < ApplicationController
-  http_basic_authenticate_with name: 'cleison', password: '123@456', except: :index
   def index; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :profiles
+  devise_for :admins
   get 'welcome/index'
   root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
