f9776dda0197568c2f9c8b09f3601ef586ce31b0
cleisommais <cleisommais@gmail.com>
2022-12-23 17:02:47 -0600

    Added changes to include the admin template

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w[admins_backoffice.css admins_backoffice.js sb-admin-2.css sb-admin-2.js]
+Rails.application.config.assets.precompile += %w[undraw_profile.svg undraw_profile_1.svg undraw_profile_2.svg undraw_profile_3.svg]
+Rails.application.config.assets.precompile += %w[undraw_posting_photo.svg undraw_rocket.svg]
