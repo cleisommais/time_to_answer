540c5d3c683f9c0f29f611295085c5a4ec1555b1
cleisommais <cleisommais@gmail.com>
2022-12-23 19:41:47 -0600

    Added users layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,6 +13,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w[admins_backoffice.css admins_backoffice.js sb-admin-2.css sb-admin-2.js]
+Rails.application.config.assets.precompile += %w[admins_backoffice.css admins_backoffice.js]
+Rails.application.config.assets.precompile += %w[users_backoffice.css users_backoffice.js]
+Rails.application.config.assets.precompile += %w[sb-admin-2.css sb-admin-2.js]
 Rails.application.config.assets.precompile += %w[undraw_profile.svg undraw_profile_1.svg undraw_profile_2.svg undraw_profile_3.svg]
 Rails.application.config.assets.precompile += %w[undraw_posting_photo.svg undraw_rocket.svg]
